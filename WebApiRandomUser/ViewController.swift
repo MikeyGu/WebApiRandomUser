75201ab99ee808a5a728a9a01bf4ac52f405c9ee
MikeyGu <mikeygu369@gmail.com>
2020-11-02 12:48:41 +0800

    Initial Commit

---
+++ WebApiRandomUser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebApiRandomUser
+//
+//  Created by Mike Gu on 2020/11/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
